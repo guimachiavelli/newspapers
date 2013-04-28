dba33f2b7fd66b433a2d795b4861f7ddbabcef25
gui machiavelli <hey@guimachiavelli.com>
2013-04-29 01:37:01 +0200

    added hour to filename

--- screen.js
+++ screen.js
@@ -4,7 +4,8 @@ var system 	= require('system'),
 var today 	= new Date(),
 	dd 		= today.getDate(),
 	mm 		= today.getMonth()+1,
-	yyyy 	= today.getFullYear();
+	yyyy 	= today.getFullYear(),
+	hour 	= today.getHours();
 
 if (dd < 10) {
 	dd = '0' + dd;
@@ -12,8 +13,11 @@ if (dd < 10) {
 if (mm < 10) {
 	mm = '0' + mm;
 }
+if (hour < 10) {
+	hour = '0' + hour;
+}
 
-today = mm + '_' + dd + '_' + yyyy;
+today = hour + '_' + dd + '_' + mm + '_' + yyyy;
 
 site_simple = system.args[1];
 site = 'http://' + system.args[1];
