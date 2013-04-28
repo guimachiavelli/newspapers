729825f050f20aeb6d355da3f7b1ea74d492ff9f
gui machiavelli <hey@guimachiavelli.com>
2013-04-28 23:36:04 +0200

    shell was calling wrong name;

--- screengiornali.sh
+++ screengiornali.sh
@@ -6,7 +6,7 @@ OUT=$(awk '{ print $2 }' $FILE)
 for site in $OUT
 do
 	#python webkit2png -F -W 1280 -H 1024 -D screenshots -d $site
-	phantomjs test.js $site 
+	phantomjs screen.js $site 
 done
 
 exit 1
