294825f517ebb258dc02b1ac0b9053361969a731
gui machiavelli <hey@guimachiavelli.com>
2013-04-29 17:39:23 +0200

    added conditionals so we can use the same script locally and on production

--- screen.js
+++ screen.js
@@ -1,11 +1,21 @@
 var system 	= require('system'),
-	page 	= require('webpage').create();
+	page 	= require('webpage').create(),
+	pro_dir = '/home/ubuntu/newspapers/screenshots/',
+	dev_dir = 'screenshots/',
+	dir 	= ''
+;
 
 var today 	= new Date(),
 	dd 		= today.getDate(),
 	mm 		= today.getMonth()+1,
 	yyyy 	= today.getFullYear(),
-	hour 	= today.getHours();
+	hour 	= today.getHours()
+;
+
+var site_simple = system.args[1],
+	server 	= system.args[2]
+;
+
 
 if (dd < 10) {
 	dd = '0' + dd;
@@ -17,16 +27,20 @@ if (hour < 10) {
 	hour = '0' + hour;
 }
 
+if (server == 'prod') {
+	dir = pro_dir;
+} else {
+	dir = dev_dir;
+}
+
 today = hour + '_' + dd + '_' + mm + '_' + yyyy;
 
-site_simple = system.args[1];
 site = 'http://' + system.args[1];
 
 page.viewportSize = { width: 1600, height: 1200 };
 
 page.open(site, function () {
-    page.render('/home/ubuntu/newspapers/screenshots/' + site_simple + '-' + today + '.png');
-	console.log(site_simple);
+    page.render(dir + site_simple + '-' + today + '.png');
 	phantom.exit();
 });
 
--- screen.sh
+++ screen.sh
@@ -1,12 +1,18 @@
 #!/bin/sh
 
-FILE="/home/ubuntu/newspapers/sites"
+if [ $1 == "prod" ] ; then
+	FILE="/home/ubuntu/newspapers/sites"
+else
+	FILE="sites"
+fi
+
+
 OUT=$(awk '{ print $2 }' $FILE)
 
 for site in $OUT
 do
 	#python webkit2png -F -W 1280 -H 1024 -D screenshots -d $site
-	phantomjs /home/ubuntu/newspapers/screen.js $site 
+	phantomjs screen.js $site $1
 	echo $site
 done
 
