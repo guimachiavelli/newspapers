b553ebe5da5df205cdaf6550c22e334f3e7531fa
gui machiavelli <hey@guimachiavelli.com>
2013-04-29 16:54:19 +0200

    more debugging for the cron task

--- screen.js
+++ screen.js
@@ -25,7 +25,7 @@ site = 'http://' + system.args[1];
 page.viewportSize = { width: 1600, height: 1200 };
 
 page.open(site, function () {
-    page.render('screenshots/' + site_simple + '-' + today + '.png');
+    page.render('/home/ubuntu/newspapers/screenshots/' + site_simple + '-' + today + '.png');
 	console.log(site_simple);
 	phantom.exit();
 });
